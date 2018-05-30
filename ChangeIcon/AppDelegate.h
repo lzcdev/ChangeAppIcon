da5c82dbd60ea7f5e2fce162677150f439eca764
lzcdev <lzc2134@gmail.com>
2018-05-30 13:55:29 +0800

    Initial project

---
+++ ChangeIcon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChangeIcon
+//
+//  Created by LuzhiChao on 2018/5/29.
+//  Copyright © 2018年 LuzhiChao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChangeIcon/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ChangeIcon
+//
+//  Created by LuzhiChao on 2018/5/29.
+//  Copyright © 2018年 LuzhiChao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
